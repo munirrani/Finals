3b2f8fd75d90ce4e72059de96d4165759391683a
Munir <munir@tuahpacket.net>
2020-01-09 15:42:54 +0800

    Q4 2018/2019

--- src/com/company/Year20182019Semester1.java
+++ src/com/company/Year20182019Semester1.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Year20182019Semester1 {
@@ -62,7 +65,45 @@ public class Year20182019Semester1 {
 
                 if (!input.contains("ATG")) break;
             }
-
         }
+
+        /*
+        Q4
+         */
+        int sentenceCount = 0, wordCount = 0;
+        int[] letterArray = new int[26];
+
+        String line = "";
+        try {
+            Scanner scanner = new Scanner(new FileInputStream(new File("myAmbition.txt")));
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                char letter;
+                int asciiIndex;
+                for (int i = 0; i < line.length(); i++) {
+                    letter = line.charAt(i);
+                    asciiIndex = letter; // A char is also an integer
+                    if (letter == '.') sentenceCount++;
+                    if (letter == ' ') wordCount++;
+                    if (asciiIndex >= 65 && asciiIndex <= 90)letterArray[asciiIndex - 65]++;
+                    if (asciiIndex >= 97 && asciiIndex <= 122) letterArray[asciiIndex - 97]++;
+                }
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        wordCount++; // Since we were only counting spaces, word count has to increment by one
+
+        System.out.println("Number of sentences: " + sentenceCount);
+        System.out.println("Number of words: " + wordCount);
+        char ascii;
+        for (int i = 0; i < letterArray.length; i++) {
+            ascii = (char) (65 + i);
+            System.out.printf(ascii + " : " + letterArray[i] + " ");
+            if ((i+1) % 8 == 0) System.out.println();
+        }
+
     }
 }
