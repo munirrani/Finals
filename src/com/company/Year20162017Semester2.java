968ec8f88ab67bee811d60f04efd982ae525c6a7
Munir <munir@tuahpacket.net>
2020-01-09 11:33:28 +0800

    Q3 2016/2017

--- src/com/company/Year20162017Semester2.java
+++ src/com/company/Year20162017Semester2.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Year20162017Semester2 {
+
+    // Q3 variables
+    static int n;
+    static int[] numbers;
 
     Year20162017Semester2() {
         /*
@@ -55,7 +60,96 @@ public class Year20162017Semester2 {
         }
         System.out.println();
 
-        
 
+        /*
+        Q3
+        Write a program that asks a user to enter an integer value, n. Based on this integer value,
+        the program should generate a set of random values within the range of 0-10000.
+        Store these values in an array. Then, create FOUR methods to perform the following actions.
+        - Display the set of integers in the array.
+        - Get the minimum number.
+        - Display the approximation of each integer value to the nearest hundred.
+        - Display each integer in the array in a reverse order.
+         */
+        scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        n = scanner.nextInt();
+
+        numbers = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            numbers[i] = random.nextInt(10000+1);
+        }
+
+        displayNumbers();
+        displayMinimumNumber();
+        displayApproximationToHundred();
+        displayInReverseOrder();
+    }
+
+    private static void displayNumbers() {
+        System.out.print("The random integer : ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static void displayMinimumNumber() {
+        int num = 10000;
+        for (int i = 0; i < n; i++) {
+            if (numbers[i] < num) num = numbers[i];
+        }
+        System.out.println("Minimum number : " + num);
+    }
+
+    private static void displayApproximationToHundred() {
+        System.out.print("The approximation of the integer to the nearest hundred : ");
+        for (int i = 0; i < n; i++) {
+            // 53 -> 100
+            // 42 -> 0
+            // 123 -> 100
+            // 450 -> 500
+            // 1234 -> 1200
+            // 10000 -> 10000
+            int digits = 0;
+            int number;
+            int tenths;
+            for (int a = numbers[i]; a > 0; a /= 10) digits++;
+
+            if (digits == 5) { // 10000 only
+                number = 10000;
+            } else if (digits <= 2) { // 0 - 99
+                if (numbers[i] < 50) {
+                    number = 0;
+                } else {
+                    number = 100;
+                }
+            } else { // digits 3 and 4
+                number = numbers[i];
+                tenths = number % 100;
+                number = number - tenths;
+                if (tenths >= 50) {
+                    number += 100;
+                }
+            }
+            System.out.print(number + " ");
+        }
+        System.out.println();
+    }
+
+    private static void displayInReverseOrder() {
+        System.out.print("The random integer in reverse order: ");
+        String value;
+        for (int i = 0; i < n; i++) {
+            value = String.valueOf(numbers[i]);
+
+            String newValue = "";
+            for (int j = value.length() - 1; j >= 0; j--) {
+                newValue = newValue.concat(String.valueOf(value.charAt(j)));
+            }
+            System.out.print(newValue + " ");
+        }
+        System.out.println();
     }
 }
